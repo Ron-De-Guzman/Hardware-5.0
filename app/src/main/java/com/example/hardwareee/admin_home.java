69b3b457014d13a012971a8a97cc6a4d185bb2c5
Mrk-Cstle <castillo.markdavid64@gmail.com>
2023-09-12 22:31:53 +0800

    .

--- app/src/main/java/com/example/hardwareee/admin_home.java
+++ app/src/main/java/com/example/hardwareee/admin_home.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class admin_home extends AppCompatActivity {
-
+//mdc
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
